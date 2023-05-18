a3ed1d89f26d84e902ab34269c54def7f0e071bc
thricvz <thricvz@gmail.com>
2023-05-18 20:51:44 +0200

    added command execution

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 import os 
 import json
 # reading the json files 
-with open('data.json', 'r') as jsonfile:
+with open('local\data.json', 'r') as jsonfile:
     data = json.load(jsonfile)
 
 class Page():
     def __init__(self,path,markdown_file):
-        #self.data = DATA["Page_commands"]
-        self.path = path
         self.file =  markdown_file
-
-    def action(self,action_type):
-        self.commands = self.data[action_type]
-        for command in self.commands:
-            command = command.replace("path",self.path)
-            command = command.replace("filename",self.file)
-            
-            os.system('powershell.exe ' + command)
 
 class Manager(Page):
     def __init__(self):
@@ -24,6 +14,18 @@ class Manager(Page):
         self.repository = self.data["git_repository"]
         self.git_user = self.data["git_credentials"]["username"]
         self.git_password = self.data["git_credentials"]["password"]
+
+        self.full_command_list = {
+            "setup git" : "local\git_setup",
+            "build template" : "local\build_template",
+            "create page" : "local\create_page.txt",
+            "delete page" : "local\delete_page.txt",
+        }
+        self.filter_list = {
+            "@username" : self.git_user,
+            "@password" : self.git_password,
+            "@repository"  : self.repository
+        }
     
     def set_up(self,repository,username,password):
         self.data["configured"] = "true"
@@ -32,7 +34,22 @@ class Manager(Page):
         self.data["git_credentials"]["password"] = password
 
         new_json_file = json.dumps(self.data,indent=4)
-        with open('data.json', 'w') as old_json_file:
+        with open('local\data.json', 'w') as old_json_file:
             old_json_file.write(new_json_file)
-        
         return self
+    
+    def manage(self,action):
+        command_list = self.full_command_list[action]
+        command_list = open(command_list,'r').readlines()
+
+        for command in command_list:
+            for string_to_filter in self.filter_list:
+                command = command.replace(string_to_filter,self.filter_list[string_to_filter])
+
+            os.system('powershell.exe ' + command)
+
+    def create_page(self,filename):
+        self.page = Page(filename)
+        self.filter_list["@filename"] = self.page.file_name 
+
+
